71857be2ca8245a615c0c94b269f22b1ebdd2b14
Praval Mishra <pravalmishra@LP-000873.local>
2025-07-24 14:35:10 +0530

    Multiple Environment changes

--- Automation/CRM2_UI/src/main/java/com/qa/crm2/factory/DriverFactory.java
+++ Automation/CRM2_UI/src/main/java/com/qa/crm2/factory/DriverFactory.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.crm2.exceptions.BrowserException;
+import com.qa.crm2.errors.AppError;
+import com.qa.crm2.exceptions.FrameworkException;
 
 public class DriverFactory {
 	 
@@ -23,6 +25,7 @@ public class DriverFactory {
 	public WebDriver initDriver(Properties prop) {
 		
 		String browserName = prop.getProperty("browser");
+		//String browserName = System.getProperty("browser"); /If pass from command line
 		highlight = prop.getProperty("highlight");
 		optionsManager = new OptionsManager(prop);
 		switch(browserName.toLowerCase().trim()) {
@@ -55,18 +58,66 @@ public class DriverFactory {
 	}
 	
 	public Properties initProp() {
-		prop =new Properties();
+		// envName = qa, stage , prod ,uat, dev
+		// mvn clean install -Denv="qa"
+        // ls -alt
+		// mvn --version
+		FileInputStream ip = null;
+		prop = new Properties();
+
+		String envName = System.getProperty("env");
+		System.out.println("Running tests on Env : " + envName);
+
 		try {
-			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
-			prop.load(ip);
+			if (envName == null) {
+				System.out.println("No env is given....hence running it on QA env...");
+				ip = new FileInputStream("./src/test/resources/config/config.qa.properties");
+
+			} else {
+
+				switch (envName.toLowerCase().trim()) {
+
+				case "qa":
+
+					ip = new FileInputStream("./src/test/resources/config/config.qa.properties");
+					break;
+
+				case "dev":
+
+					ip = new FileInputStream("./src/test/resources/config/config.dev.properties");
+					break;
+
+				case "stage":
+
+					ip = new FileInputStream("./src/test/resources/config/config.stage.properties");
+					break;
+
+				case "uat":
+
+					ip = new FileInputStream("./src/test/resources/config/config.uat.properties");
+					break;
+
+				case "prod":
+
+					ip = new FileInputStream("./src/test/resources/config/config.config.properties");
+					break;
+
+				default:
+					System.out.println("Please pass the right env name..... " + envName);
+					throw new FrameworkException(AppError.ENV_NAME_NOT_FOUND + " : " + envName);
+				}
+
+			}
 		} catch (FileNotFoundException e) {
-		
 			e.printStackTrace();
+		}
+
+		try {
+			prop.load(ip);
 		} catch (IOException e) {
-			
 			e.printStackTrace();
 		}
 		return prop;
-	}
 
-}
+	}
+	}
